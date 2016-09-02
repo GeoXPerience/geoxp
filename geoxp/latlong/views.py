b56d6c7bb9cb4ff7a4e741f6edfaf7ca93077585
holypriest <chemistoid@gmail.com>
2016-09-02 15:42:46 -0300

    Upload form implemented

--- geoxp/latlong/views.py
+++ geoxp/latlong/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import UploadFileForm
 
 # Create your views here.
+def upload(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            #this method will be implemented later
+            #handle_uploaded_file(request.FILES[''])
+            return HttpResponseRedirect('/success')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
