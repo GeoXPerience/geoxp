013d48a7f49cd4656196a0bd64d50d78f4e014d0
Isabella Mendonça <isaisaisa@live.dk>
2016-09-02 22:43:53 -0300

    Ignoring header in CSV files

--- geoxp/modules/csv_handler.py
+++ geoxp/modules/csv_handler.py
@@ -1,17 +1,22 @@
-#!/usr/bin/env python
+"""CSV Handler."""
 import csv
-import geocoder
 import sys
 
-def read_csv(file_name):
-    # Tratar primeira linha
-    csv_data = None
 
+def read_csv(file_name):
+    """Open CSV file."""
     with open(file_name, 'rb') as csv_file:
+        header = csv.Sniffer().has_header(csv_file.read(1024))
+        csv_file.seek(0)
         csv_data = csv.reader(csv_file)
+        if header:
+            next(csv_data)
+        print list(csv_data)
+
 
 def main():
-    csv_data = read_csv(sys.argv[1])
+    """Main."""
+    read_csv(sys.argv[1])
 
 if __name__ == '__main__':
     main()
