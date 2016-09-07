7826679d9a91d5058519c1aaa3e8f4a58fef45a4
holypriest <chemistoid@gmail.com>
2016-09-06 23:34:43 -0300

    CSV handler working

--- geoxp/modules/csv_handler.py
+++ geoxp/modules/csv_handler.py
@@ -1,30 +1,21 @@
 '''This module handles CSV files.'''
 import csv
 import sys
+from location import Location
 
 
-def build_locations(csv_data):
-    '''This function builds a lists of location objects.'''
-    pass
-
-
-def read_csv(file_name):
-    '''This function reads a CSV file.'''
-    with open(file_name, 'rb') as csv_file:
-        header = csv.Sniffer().has_header(csv_file.read(4096))
-        csv_file.seek(0)
-        csv_data = csv.reader(csv_file)
-        if header:
-            next(csv_data)
-        csv_as_list = [row for row in csv_data]
-
-    return csv_as_list
+def geocode_uploaded_file(input_file, output_file):
+    csv_data = csv.reader(input_file, delimiter=';')
+    next(csv_data)
+    for address in csv_data:
+        loc = Location(address)
+        loc.write(output_file)
 
 
 def main():
     '''This is functions is for testing purposes.'''
-    csv_data = read_csv(sys.argv[1])
-    build_locations(csv_data)
+    with open('addresses.csv', 'rb') as input_file, open('output.csv', 'a') as output_file:
+        geocode_uploaded_file(input_file, output_file)
 
 if __name__ == '__main__':
     main()
