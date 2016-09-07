381702b37ff2dde3fd3ede29303f158be4b2ce8f
holypriest <chemistoid@gmail.com>
2016-09-07 14:35:39 -0300

    Now downloading output.csv

--- geoxp/latlong/views.py
+++ geoxp/latlong/views.py
@@ -9,9 +9,12 @@ def upload(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            with open('./media/output.csv', 'a') as output_file:
+            with open('./media/output.csv', 'w') as output_file:
                 geocode_uploaded_file(request.FILES['file'], output_file)
-            return HttpResponse("Sucesso")
+            with open('./media/output.csv', 'rb') as download_file:
+                response = HttpResponse(download_file.read(), content_type="text/csv")
+                response['Content-Disposition'] = 'inline; filename=output.csv'
+                return response
     else:
         form = UploadFileForm()
     return render(request, 'upload.html', {'form': form})
