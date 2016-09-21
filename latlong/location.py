e928b6366709f7f5b25731002b447a7ddda7aec6
Ian Carvalho <iancarv@gmail.com>
2016-09-21 10:53:46 -0300

    Added tests for geocode uploaded function and zip formatting

--- latlong/location.py
+++ latlong/location.py
@@ -5,13 +5,13 @@ import geocoder
 class Location(object):
     '''Location is a model for representing a physical location'''
 
-    def __init__(self, address):
+    def __init__(self, address, geoloc=None):
         '''Initialize an Location with an address, number, zip code
         and geolocation'''
         self.street = address[0]
         self.number = address[1]
         self.zip = address[2]
-        self.geoloc = self.geocode(address)
+        self.geoloc = geoloc or self.geocode(address)
 
     def geocode(self, address):
         loc = '%s, %s - %s' % (address[0], address[1], address[2])
@@ -30,3 +30,12 @@ class Location(object):
     def __unicode__(self):
         return '%s, %s - %s (%s, %s)' % (self.street, self.number, self.zip,
                                          self.geoloc.lat, self.geoloc.long)
+
+    def __eq__(self, other):
+        """Override the default Equals behavior"""
+        return (self.geoloc.lat == other.geoloc.lat and
+                self.geoloc.long == other.geoloc.long)
+
+    def __ne__(self, other):
+        """Define a non-equality test"""
+        return not self.__eq__(other)
